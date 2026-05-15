55b413f247140267909b94773ec774de0d4d4c08
Ruthvik34 <ruthvikwadkar@gmail.com>
2026-05-15 19:20:39 +0530

    Added the Important changes

--- app/src/main/java/com/ruthvik/musicplayer/SplashActivity.kt
+++ app/src/main/java/com/ruthvik/musicplayer/SplashActivity.kt
@@ -18,6 +18,7 @@ class SplashActivity : AppCompatActivity() {
 
         binding.btnStart.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
